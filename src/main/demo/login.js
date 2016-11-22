7131dfa77b02c926996877b0331b77e368745d5d
赵鹏飞 <pengfeizhao9@creditease.cn>
2016-11-22 19:30:15 +0800

    create project

---
+++ src/main/demo/login.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Administrator on 2016/11/14.
+ */
+
+
+
+        console.log("success");
+
+
+
+
+function login() {
+
+    var username = document.getElementById("username").firstElementChild;
+    var password = document.getElementById("password").firstElementChild;
+
+    var logJson = {username: username, password: password};
+
+
+    $.ajax(
+        {
+            type: "POST",
+            url: "https://www.baidu.com",
+            contentType: "application/json;charset=utf-8",
+            data: JSON.stringify(logJson),
+            dataType: "json",
+
+        }
+    )
+}
+
